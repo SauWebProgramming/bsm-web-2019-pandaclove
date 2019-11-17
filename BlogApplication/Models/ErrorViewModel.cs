e1443eb2c422a69fee993a102a8a0cab34bdfda5
Ebubekir Karanfil <karanfilebubekir@hotmail.com>
2019-11-17 12:17:17 +0300

    Proje oluşturuldu

---
+++ BlogApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogApplication.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
