ab583f76c79c86133579a25f8f4d3ba6dd855573
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2019-12-23 05:02:17 +0300

    Role silme işlemi yapıdlı.

--- ShowApp/ShowApp.WebUI/Areas/Admin/Controllers/UserRoleController.cs
+++ ShowApp/ShowApp.WebUI/Areas/Admin/Controllers/UserRoleController.cs
@@ -44,5 +44,27 @@ namespace ShowApp.WebUI.Areas.Admin.Controllers
             }
             return View(name);
         }
+        [HttpPost]
+        public async  Task<IActionResult> Delete(string id)
+        {
+            var role = await RoleManager.FindByIdAsync(id);
+            if(role != null)
+            {
+                var result = await RoleManager.DeleteAsync(role);
+                if(result.Succeeded)
+                {
+                    TempData["message"] = $"{role.Name} has been deleted.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
